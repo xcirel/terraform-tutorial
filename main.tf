23d8db6e8636fcf62abc7aeb2e097d1db41c278a
Eric Santos <eric.santos@newrizon.global>
2023-05-19 18:48:16 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-007855ac798b5175e"
+  instance_type = "t2.micro"
+  key_name = "devops"
+
+  tags = {
+    Name = "ExampleAppServerInstance2"
+  }
+}
